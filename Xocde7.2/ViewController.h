c139ebedd8690a45d71a9c1595cfb24e413a42a6
wangli <you@example.com>
2017-01-16 11:28:20 +0800

    Initial Commit

---
+++ Xocde7.2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Xocde7.2
+//
+//  Created by Ernie Liu on 17/1/16.
+//  Copyright © 2017年 Ernie Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Xocde7.2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Xocde7.2
+//
+//  Created by Ernie Liu on 17/1/16.
+//  Copyright © 2017年 Ernie Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
